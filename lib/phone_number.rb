053b2ef8c87ae0017d18b3cdfdcdb98771747069
Simon Huerlimann <Simon Huerlimann simon.huerlimann@cyt.ch>
2009-08-03 23:03:37 +0200

    Move PhoneNumber out of Vcards namespace.

---
+++ lib/phone_number.rb
@@ -0,0 +1,18 @@
+class PhoneNumber < ActiveRecord::Base
+  belongs_to :vcard
+
+  def to_s
+    case phone_number_type
+    when 'phone'
+      return "Tel: #{number}"
+    when 'fax'
+      return "Fax: #{number}"
+    when 'mobile'
+      return "Mob: #{number}"
+    when 'email'
+      return "Mail: #{number}"
+    else
+      return number
+    end
+  end
+end
