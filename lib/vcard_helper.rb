8ab5169a136124149ccc3a72a28fd95b16a5c5c3
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-09-16 16:19:36 +0200

    Mark VcardHelper method output as html_safe.

--- lib/vcard_helper.rb
+++ lib/vcard_helper.rb
@@ -1,15 +1,15 @@
 module VcardHelper
   module InstanceMethods
     def address(vcard, line_separator = '<br/>')
-      vcard.address_lines.map{|line| h(line)}.join(line_separator)
+      vcard.address_lines.map{|line| h(line)}.join(line_separator).html_safe
     end
 
     def full_address(vcard, line_separator = '<br/>')
-      vcard.full_address_lines.map{|line| h(line)}.join(line_separator)
+      vcard.full_address_lines.map{|line| h(line)}.join(line_separator).html_safe
     end
 
     def contact(vcard, line_separator = '<br/>', label_separator = ' ')
-      vcard.contact_lines(label_separator).map{|line| h(line)}.join(line_separator)
+      vcard.contact_lines(label_separator).map{|line| h(line)}.join(line_separator).html_safe
     end
   end
 end
