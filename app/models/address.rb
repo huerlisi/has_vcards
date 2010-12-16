e313df0b614472f91c4567c5a6428557d3d8630e
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-12-16 08:52:00 +0100

    Add .to_s to Address model.

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,12 @@
 class Address < ActiveRecord::Base
   belongs_to :vcard
+  
+  # Helpers
+  def to_s
+    I18n.translate('has_vcards.address.to_s',
+      :street_address => street_address,
+      :postal_code => postal_code,
+      :locality => locality
+    )
+  end
 end
