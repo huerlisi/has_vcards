646802c724cbe4d9f1d0a554cb378e8b4043c235
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-11-21 13:30:22 +0100

    Drop type specific relations from Vcards. Use contacts scopes.

--- app/models/vcard.rb
+++ app/models/vcard.rb
@@ -74,48 +74,6 @@ class Vcard < ActiveRecord::Base
   # Phone numbers
   has_many :contacts, :class_name => 'PhoneNumber', :as => :object
   accepts_nested_attributes_for :contacts
-
-  has_many :phone_numbers, :class_name => 'PhoneNumber', :as => :object, :conditions => ["phone_number_type = ?", 'phone'], :after_add => :add_phone_number
-  def add_phone_number(number)
-      number.phone_number_type = 'phone'
-      number.save
-  end
-
-  def phone_number
-    phone_numbers.first
-  end
-  
-  def phone_number=(number)
-    phone_numbers.build(:number => number, :phone_number_type => 'phone')
-  end
-  
-  has_many :mobile_numbers, :class_name => 'PhoneNumber', :as => :object, :conditions => ["phone_number_type = ?", 'mobile'], :after_add => :add_mobile_number
-  def add_mobile_number(number)
-      number.phone_number_type = 'mobile'
-      number.save
-  end
-
-  def mobile_number
-    mobile_numbers.first
-  end
-  
-  def mobile_number=(number)
-    mobile_numbers.build(:number => number, :phone_number_type => 'mobile')
-  end
-  
-  has_many :fax_numbers, :class_name => 'PhoneNumber', :as => :object, :conditions => ["phone_number_type = ?", 'fax'], :after_add => :add_fax_number
-  def add_fax_number(number)
-      number.phone_number_type = 'fax'
-      number.save
-  end
-
-  def fax_number
-    fax_numbers.first
-  end
-  
-  def fax_number=(number)
-    fax_numbers.build(:number => number, :phone_number_type => 'fax')
-  end
   
   # Salutation
   def salutation
