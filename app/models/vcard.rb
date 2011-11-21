39639122954b9d3b9f0b3db334f763ca26e630fe
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-11-21 13:32:53 +0100

    Reject contacts with number not set as nested attributes to vcard.

--- app/models/vcard.rb
+++ app/models/vcard.rb
@@ -73,7 +73,7 @@ class Vcard < ActiveRecord::Base
 
   # Phone numbers
   has_many :contacts, :class_name => 'PhoneNumber', :as => :object
-  accepts_nested_attributes_for :contacts
+  accepts_nested_attributes_for :contacts, :reject_if => proc {|attributes| attributes['number'].blank? }
   
   # Salutation
   def salutation
