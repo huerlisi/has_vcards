f1f93b733b861f32d95dd7c77d0e4c8237027415
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2012-04-23 15:42:35 +0200

    Fix class name in create_vcard_tables migration.

--- db/migrate/000_create_vcard_tables.rb
+++ db/migrate/000_create_vcard_tables.rb
@@ -1,4 +1,4 @@
-class CreateHasVcards < ActiveRecord::Migration
+class CreateVcardTables < ActiveRecord::Migration
   def self.up
     create_table 'addresses', :force => true do |t|
       t.string  'post_office_box',  :limit => 50
