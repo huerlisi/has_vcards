8b1c9c4e22ee729a3870cd294d02f84cbe21c15a
Simon Hürlimann <simon.huerlimann@cyt.ch>
2014-06-16 23:58:12 +0200

    Initialize rspec.

---
+++ spec/rails_helper.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+ENV['RAILS_ENV'] ||= 'test'
+
+require File.expand_path("../dummy/config/environment.rb",  __FILE__)
+require 'rspec/rails'
+require 'factory_girl_rails'
+
+Rails.backtrace_cleaner.remove_silencers!
+
+# Load support files
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
+
+RSpec.configure do |config|
+  config.mock_with :rspec
+  config.use_transactional_fixtures = true
+end
