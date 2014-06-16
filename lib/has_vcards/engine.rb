6864250f466d35b2a430df037cd59130033faaab
Simon Hürlimann <simon.huerlimann@cyt.ch>
2014-06-16 22:38:44 +0200

    Rename Railtie to Engine and drop name.

--- lib/has_vcards/engine.rb
+++ lib/has_vcards/engine.rb
@@ -5,14 +5,10 @@ require 'simple_form'
 require 'i18n_rails_helpers'
 
 module HasVcards
-  class Railtie < Rails::Engine
-    engine_name "has_vcards"
-
+  class Engine < Rails::Engine
     initializer :after_initialize do
       ActionController::Base.helper HasVcardsHelper
-
       ActiveRecord::Base.extend(HasVcardsClassMethods)
-
       SwissMatch::Address.send :include, Vcard::DirectoryAddress if defined?(SwissMatch::Address)
     end
   end
